ff8805888aba338ef60c444f784989fcdefa3f05
Demyan228 <bubnovdema@gmail.com>
2022-03-02 16:57:29 +0800

    ans3

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+professions = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+                   'инженер по терраформированию', 'климатолог',
+                   'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+                   'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+                   'штурман', 'пилот дронов']
 
 
 @app.route('/<title>')
@@ -12,6 +17,11 @@ def index(title):
 @app.route('/training/<prof>')
 def training(prof):
     return render_template("index.html", prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template("list_prof.html", names=professions, list=list)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
